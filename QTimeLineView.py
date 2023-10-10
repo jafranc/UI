165aa80b37a8f9a164b84954868221a06ec04105
jfranc <jfranc@stanford.edu>
2023-10-10 08:06:53 -0700

    wip correction

--- QTimeLineView.py
+++ QTimeLineView.py
@@ -1,24 +1,25 @@
-from PyQt5.QtCore import QPersistentModelIndex, QPoint, QRect, QEvent, QModelIndex, Qt
-from PyQt5.QtGui import QHoverEvent, QHelpEvent, QPainter, QPalette, QPen, QColor, QColorConstants
+from PyQt5.QtCore import QPersistentModelIndex, QPoint, QRect, QEvent, QModelIndex, Qt, QRectF
+from PyQt5.QtGui import QHoverEvent, QHelpEvent, QPainter, QPalette, QPen, QColor, QColorConstants, QRegion
 from PyQt5.QtWidgets import (QListView,
                              QAbstractItemView,
                              QTableView,
                              QTreeView, QStyleOptionViewItem, QStyle, QAbstractScrollArea)
 
-import QTimeLineItemDelegate
+from QTimeLineItemDelegate import QTimeLineItemDelegate
+
 
 class QTimeLineView(QAbstractItemView):
-    hoverIndex: QPersistentModelIndex
-    scrollOffset: QPoint
-
-    scale: float = 1.
-    timestempSectionHeight: int = 30
-    timestampPer100Pixels: int = 1.0
-    layerHeight: int = 20
 
     def __init__(self):
 
         super().__init__()
+        self.hoverIndex = QPersistentModelIndex()
+        self.scrollOffset = QPoint()
+
+        self.scale: float = 1.
+        self.timestempSectionHeight: int = 30
+        self.timestampPer100Pixels: int = 1.0
+        self.layerHeight: int = 20
 
         self.viewport().setAttribute(Qt.WA_Hover)
         self.setItemDelegate(QTimeLineItemDelegate())
@@ -37,7 +38,7 @@ class QTimeLineView(QAbstractItemView):
         painter = QPainter(self.viewport())
         painter.setRenderHint(QPainter.Antialiasing, True)
 
-        timestempsRectIntersection = a0.rect() & QRect(0,0, a0.rect().width(), self.timestempSectionHeight)
+        timestempsRectIntersection = a0.rect() & QRect(0, 0, a0.rect().width(), self.timestempSectionHeight)
         if not timestempsRectIntersection.isEmpty():
             painter.setBrush(self.palette().color(QPalette.Window).lighter(130))
             painter.setPen(QColorConstants.Black)
@@ -45,23 +46,23 @@ class QTimeLineView(QAbstractItemView):
             painter.fillRect(timestempsRectIntersection, painter.brush())
             painter.drawLine(0, self.timestempSectionHeight, a0.rect().width(), self.timestempSectionHeight)
 
-            painter.setPen(QPen(self.palette().color(QPalette.WindowText),1))
-            for i in range(int(self.scrollOffset.x()/100 + 1)*100, a0.rect().width()+ self.scrollOffset.x() ,100):
-                text = '{}s'.format(self.pixelsToDuration(i).count() * 1e6)
+            painter.setPen(QPen(self.palette().color(QPalette.WindowText), 1))
+            for i in range(int(self.scrollOffset.x()/ 100 + 1) * 100, a0.rect().width() + self.scrollOffset.x(), 100):
+                text = '{}s'.format(self.pixelsToDuration(i))
                 textRect = painter.fontMetrics().boundingRect(text)
 
-                textRect.translate(-self.scrollOffset.x(),0)
-                text.translate( i - textRect.width()/2, self.timestempSectionHeight - 13)
-                if textRect.right() > a0.rect().width() -5 :
-                    textRect.translate(a0.rect().width() - textRect.right() -5 ,0)
+                textRect.translate(-self.scrollOffset.x(), 0)
+                textRect.translate(int(i - textRect.width() / 2), int(self.timestempSectionHeight - 13))
+                if textRect.right() > a0.rect().width() - 5:
+                    textRect.translate(a0.rect().width() - textRect.right() - 5, 0)
                 elif textRect.left():
-                    textRect.translate(textRect.left()+5,0)
+                    textRect.translate(textRect.left() + 5, 0)
 
                 painter.drawLine(i - self.scrollOffset.x(),
                                  self.timestempSectionHeight - 10,
-                                 i- self.scrollOffset.x(),
+                                 i - self.scrollOffset.x(),
                                  self.timestempSectionHeight)
-                painter.drawText(textRect, text)
+                painter.drawText(QRectF(textRect), text)
 
             option = QStyleOptionViewItem()
             for i in range(self.model().rowCount()):
@@ -72,56 +73,63 @@ class QTimeLineView(QAbstractItemView):
                 if horizontalSeparatorLineY > self.viewport().height() + self.layerHeight:
                     break
 
-                item = self.model().index(i,0)
-                bgPenColor = item.data(Qt.DecorationRole).value
-                bgFillColor = ...
+                item = self.model().index(i, 0)
+                # bgPenColor = QColor(item.data(Qt.DecorationRole).value).darker(200)
+                # bgFillColor = QColor(item.data(Qt.DecorationRole).value).darker(150)
+                #replace colors
+                bgPenColor = QColor(QColorConstants.Red)
+                bgFillColor = QColor(QColorConstants.Blue)
                 rectWithoutTimeStamps = a0.rect()
                 rectWithoutTimeStamps.setTop(self.timestempSectionHeight)
                 painter.setPen(bgPenColor)
-                painter.fillRect(QRect(0, i*self.layerHeight + self.timestempSectionHeight - self.scrollOffset.y(),
-                                       rectWithoutTimeStamps.width(), self.layerHeight) & rectWithoutTimeStamps, bgFillColor)
+                painter.fillRect(QRect(0, i * self.layerHeight + self.timestempSectionHeight - self.scrollOffset.y(),
+                                       rectWithoutTimeStamps.width(), self.layerHeight) & rectWithoutTimeStamps,
+                                 bgFillColor)
                 painter.drawLine(0, horizontalSeparatorLineY, rectWithoutTimeStamps.width(), horizontalSeparatorLineY)
-                for j in range(self.model().columnCount()):
-                    segment = self.model().index(i,j)
-                    if not segment.isValid():
+                for j in range(1,self.model().columnCount()):
+                    segment = self.model().index(i, j)
+                    if not segment.isValid() or (segment.data(Qt.UserRole + 1) is None) or (segment.data(Qt.UserRole + 2) is None):
                         continue
                     option.rect = self.visualRect(segment)
                     # ???
-                    if segment == self.hoverIndex:
-                        option.state.State_MouseOver
+                    option.state.State_MouseOver = (segment == self.hoverIndex)
 
                     if option.rect.intersects(rectWithoutTimeStamps):
                         option.rect = option.rect & rectWithoutTimeStamps
-                        self.itemDelegate().paint(painter,option,segment)
-            for i in range( int((a0.rect().left() - self.scrollOffset.x()) /100 + 1) * 100,
-                            a0.rect().right() + self.scrollOffset.x(), 100):
-                painter.setPen(QColor(0,0,0,50))
-                painter.drawLine(i - self.scrollOffset.x(),
-                                 self.timestempSectionHeight if (self.timestempSectionHeight > a0.rect().top()) else a0.rect().top(),
-                                 i - self.scrollOffset.x(),
-                                 a0.rect().bottom())
-
+                        self.itemDelegate().paint(painter, option, segment)
+            for i in range(int((a0.rect().left() - self.scrollOffset.x()) / 100 + 1) * 100,
+                           a0.rect().right() + self.scrollOffset.x(), 100):
+                    painter.setPen(QColor(0, 0, 0, 50))
+                    painter.drawLine(i - self.scrollOffset.x(),
+                                     self.timestempSectionHeight if (
+                                             self.timestempSectionHeight > a0.rect().top()) else a0.rect().top(),
+                                     i - self.scrollOffset.x(),
+                                     a0.rect().bottom())
 
     def resizeEvent(self, e):
         self.updateScrollBars()
-        QAbstractItemView.resizeEvent(e)
+        super().resizeEvent(e)
 
     def showEvent(self, a0):
         self.updateScrollBars()
-        QAbstractItemView.showEvent(a0)
+        super().showEvent(a0)
 
     def indexAt(self, p):
-        row = (p.y() - self.timestempSectionHeight) / self.layerHeight
-        for i in range(self.model().columnCount()):
-            if self.visualRect(self.model().index(row,i)).contains(p):
-                return self.model().index(row,i)
-        return
+        row = max(0, (p.y() - self.timestempSectionHeight) / self.layerHeight)
+        row = min(row, self.model().rowCount()-1)
+        for i in range(1,self.model().columnCount()):
+            if self.visualRect(self.model().index(int(row), i)).contains(p):
+                return self.model().index(int(row), i)
+        return QModelIndex()
 
     def scrollTo(self, index, hint=None):
         return
 
     def visualRect(self, index):
-        return self.itemRect(index).translated(- self.scrollOffset)
+        if (index.row() >= 0 ) and (index.row() < index.model().rowCount() ) :
+            return self.itemRect(index).translated(- self.scrollOffset)
+        else:
+            return QRect()
 
     def horizontalOffset(self):
         return 0
@@ -130,7 +138,7 @@ class QTimeLineView(QAbstractItemView):
         return False
 
     def moveCursor(self, cursorAction, modifiers):
-        return
+        return QModelIndex()
 
     def setSelection(self, rect, command):
         return
@@ -139,17 +147,19 @@ class QTimeLineView(QAbstractItemView):
         return 0
 
     def visualRegionForSelection(self, selection):
-        return
+        return QRegion()
 
-    def viewportEvent(self, e : QHoverEvent):
-        if e.type() in [QEvent.HoverMode, QEvent.HoverEnter]:
-            self.update(self.hoverIndex)
+    def viewportEvent(self, e: QHoverEvent):
+        if e.type() in [QEvent.HoverMove, QEvent.HoverEnter]:
+            self.update(QModelIndex(self.hoverIndex))
             self.hoverIndex = self.indexAt(e.pos())
             if self.hoverIndex.isValid():
                 self.hoverIndex = self.indexAt(e.pos())
+                val = self.hoverIndex.data(Qt.UserRole + 1)
+                self.hoverIndex.model().setData(self.hoverIndex,val+100, Qt.UserRole+1)
                 self.update(self.hoverIndex)
         elif e.type() in [QEvent.HoverLeave]:
-            self.update(self.hoverIndex)
+            self.update(QModelIndex(self.hoverIndex))
             self.hoverIndex = QModelIndex()
             self.update(self.hoverIndex)
         elif e.type() in [QEvent.ToolTip, QEvent.QueryWhatsThis, QEvent.WhatsThis]:
@@ -157,43 +167,44 @@ class QTimeLineView(QAbstractItemView):
             index = self.indexAt(he.pos())
             option = QStyleOptionViewItem()
             option.rect = self.visualRect(index).translated(-self.scrollOffset.x(), -self.scrollOffset.y())
-            option.state |= QStyle.State_HasFocus if ( index == self.currentIndex() ) else QStyle.State_None
+            option.state |= QStyle.State_HasFocus if (index == self.currentIndex()) else QStyle.State_None
 
-            delegate = self.itemDelegateForIndex(index)
+            delegate = self.itemDelegateForRow(index.row())
             if not delegate:
                 return False
-            return delegate.helpEvent(he,self,option,index)
-
-        return QAbstractScrollArea.viewportEvent(e)
+            return delegate.helpEvent(he, self, option, index)
+        #
+        # return QAbstractScrollArea.viewportEvent(e)
+        return super().viewportEvent(e)
 
     # QAbstactScrollArea
     def scrollContentsBy(self, dx, dy):
-        self.scrollOffset -= QPoint(dx,dy)
-        QAbstractItemView.scrollContentsBy(dx,dy)
+        self.scrollOffset -= QPoint(dx, dy)
+        QAbstractItemView.scrollContentsBy(dx, dy)
 
     def itemRect(self, index):
-        startTime = index.data( Qt.UserRole + 1).toDouble()
-        duration = index.data(Qt.UserRole + 2).toDouble()
+        startTime = index.data(Qt.UserRole + 1)
+        duration = index.data(Qt.UserRole + 2)
         x = self.durationToPixels(startTime)
         width = self.durationToPixels(duration)
-        return QRect(x, index.row()* self.layerHeight + self.timestempSectionHeight, self.width(), self.layerHeight )
+        return QRect(int(x), index.row() * self.layerHeight + self.timestempSectionHeight, int(width), self.layerHeight)
 
     def updateScrollBars(self):
         if self.model() is None:
             return
         max_ = 0
-        for i in range(self.model().rowCount()):
+        for i in range(1,self.model().rowCount()):
             item = self.model().index(i, self.model().columnCount() - 1)
             if not item.isValid():
                 continue
-            max_ = max(self.itemRect(item) - self.viewport().width())
+            max_ = max(max_,self.itemRect(item).right() - self.viewport().width())
 
         self.horizontalScrollBar().setRange(0, max_)
         self.verticalScrollBar().setRange(0,
                                           self.model().rowCount() * self.layerHeight + self.timestempSectionHeight - self.viewport().height())
 
     def durationToPixels(self, val):
-        return val * self.scale * 1000
+        return val * self.scale
 
     def pixelsToDuration(self, val):
-        return val / self.scale / 1000
+        return val / self.scale
--- test_timeline.py
+++ test_timeline.py
@@ -1,11 +1,65 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import Qt, QTimeLine
+from PyQt5.QtGui import QStandardItemModel, QStandardItem, QGuiApplication, QColorConstants
+from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QTabWidget, QProgressBar, QGraphicsView, QApplication
+
+from QTimeLineView import QTimeLineView
 
 
 class MainWindow(QMainWindow):
 
-    def __summy_(self):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("GEOS standard UI")
+        self.tabWidget = QTabWidget()
+        self.setCentralWidget(self.tabWidget)
+
+    def add_timeline(self):
+
+        # by hand
+        timeline = QTimeLineView()
+
+        timeline.setModel( QStandardItemModel(timeline) )
+        timeline.model().clear()
+        timeline.setScale(1.0)
+
+        layer = QStandardItem("tada")
+        layer.setData(QColorConstants.Black, Qt.DecorationRole)
+        layer.setData("layer", Qt.ToolTipRole)
+        timeline.model().appendRow(layer)
+
+        section = QStandardItem("SECTION")
+        section.setData(QColorConstants.Yellow, Qt.DecorationRole)
+        section.setData("sec_data", Qt.ToolTipRole)
+        section.setData(12.0, Qt.UserRole + 1)
+        section.setData(122.0, Qt.UserRole + 2)
+
+
+        timeline.model().setItem(layer.row(), 1, section)
+
+
+        ## new layer
+
+        layer2 = QStandardItem("todo")
+        layer2.setData(QColorConstants.Black, Qt.DecorationRole)
+        layer2.setData("layer-2", Qt.ToolTipRole)
+        timeline.model().appendRow(layer2)
+
+        section = QStandardItem("SECTION-2")
+        section.setData(QColorConstants.Red, Qt.DecorationRole)
+        section.setData("sec2_data", Qt.ToolTipRole)
+        section.setData(145.0, Qt.UserRole + 1)
+        section.setData(222.0, Qt.UserRole + 2)
+
+
+        timeline.model().setItem(layer2.row(), 1, section)
+
+        timeline.show()
+
+        self.tabWidget.addTab(timeline, "toto")
+
 
 
 if __name__ == "__main__":
@@ -13,5 +67,6 @@ if __name__ == "__main__":
 
     window = MainWindow()
     window.show()
+    window.add_timeline()
 
     app.exec()
